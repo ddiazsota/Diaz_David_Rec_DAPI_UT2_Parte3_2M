82e7cdf44dcfe82112fa02b8f128a519778bbba3
ddiazsota <144794611+ddiazsota@users.noreply.github.com>
2024-05-13 18:31:42 +0200

    Add files via upload

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,4 +1,17 @@
 def calculate_grade(Practica01, Practica02, Practica03, Examen, Recuperacion, Actitud):
+    """Esta función consiste en que a partir de los datos de proccess class 
+    calcule la nota media de los alumnos/as y función recibirá las notas de todos 
+    los apartados evaluados de cada alumno/a y devolverá unanota final y un valor Verdadero/Falso
+    en función de si ha aprobado o suspendido.
+    NotaFinal = (Practica01 + Practica02 +Practica03)/3 * 0,3 +
+    max(Examen; Recuperacion)* 0,6 + Actitud * 0,1
+    Parámetros:
+        -Fichero class.csv
+        -Seis float con las notas de cada alumno:
+         Practica01, Practica02, Practica03, Examen, Recuperacion, Actitud
+        Salida:
+            -Asigatura superada o no
+    """
     Examen2 = [Examen, Recuperacion]
     total_practicas = Practica01 + Practica02 + Practica03
     NotaFinal = total_practicas/3 * 0.3 + max(Examen2)* 0.6 + Actitud * 0.1
